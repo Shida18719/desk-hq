7afdae242dd8e3c1bb3c5d8153958761d2e77cc1
Rashidat Adekoya <rawsheedah@yahoo.com>
2023-03-10 20:25:21 +0000

    create booking models form

---
+++ desk/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Booking
+
+
+class BookingForm(forms.ModelForm):
+    """
+    Create BookingForm
+    """
+
+    class Meta:
+        model = Booking
+        fields = [
+            'location', 'space_booking', 'booking_date',
+            'booking_duration', 'booking_start', 'booking_end']
