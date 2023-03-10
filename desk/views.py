b5cf39d7800c2c8d6708aa6bb08b41d979785bfb
Rashidat Adekoya <rawsheedah@yahoo.com>
2023-03-10 14:19:16 +0000

    Add html files to templates

--- desk/views.py
+++ desk/views.py
@@ -8,8 +8,12 @@ class BookingListView(generic.ListView):
     View for displaying booking availability.
     """
     model = Booking
-    queryset = Booking().objects.filter(client=self.request.user)
     template_name = 'spaces.html'
     context_object_name = 'spaces'
     paginate_by = 4
 
+    def get_queryset(self):
+        """
+        Filters bookings for the current user
+        """
+        return Booking.objects.filter(client=self.request.user)
