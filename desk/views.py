35999f9b54943073261cd366162694496685235e
Rashidat Adekoya <rawsheedah@yahoo.com>
2023-03-10 13:53:05 +0000

    Create BookingListView for booking display in views.py

--- desk/views.py
+++ desk/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Booking
 
-# Create your views here.
+
+class BookingListView(generic.ListView):
+    """
+    View for displaying booking availability.
+    """
+    model = Booking
+    queryset = Booking().objects.filter(client=self.request.user)
+    template_name = 'spaces.html'
+    context_object_name = 'spaces'
+    paginate_by = 4
+
