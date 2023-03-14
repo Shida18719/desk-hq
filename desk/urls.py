9226717f89d684bdcf48eaec962dc665d18d4478
Rashidat Adekoya <rawsheedah@yahoo.com>
2023-03-14 08:48:07 +0000

    Create templates views and wire up urls

--- desk/urls.py
+++ desk/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 
+# Urls for app views
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
-    path('book_space/', views.BookingFormView.as_view(), name='book_space'),
+    # path('book_space/', views.BookingFormView.as_view(), name='book_space'),
+    path('space_booking/', views.BookingFormView.as_view(), name='space_booking'),
+    path('about_us/', views.AboutView.as_view(), name="about_us"),
 ]
--- desk/views.py
+++ desk/views.py
@@ -1,19 +1,51 @@
 from django.shortcuts import render
-from django.views import generic
+from django.http import HttpResponseRedirect
+from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+
+# Booking
 from .models import Booking
+from .forms import BookingForm
+from django.views.generic.edit import FormView
 
 
-class BookingListView(generic.ListView):
+class HomePageView(TemplateView):
+    """
+    View for home page.
+    """
+    template_name = 'home/index.html'
+
+# def home_page(request):
+#     return render(request, 'index.html')
+
+
+class BookingFormView(FormView):
     """
     View for displaying booking availability.
     """
     model = Booking
-    template_name = 'spaces.html'
-    context_object_name = 'spaces'
-    paginate_by = 4
+    form_class = BookingForm
+    template_name = 'desk/space_booking.html'
 
-    def get_queryset(self):
-        """
-        Filters bookings for the current user
-        """
-        return Booking.objects.filter(client=self.request.user)
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            # <process form cleaned data>
+            form.save()
+            return HttpResponseRedirect('/')
+
+        return render(request, self.template_name, {'form': form})
+
+
+class AboutView(TemplateView):
+    """
+    View for about_us page
+    """
+    template_name = 'home/about_us.html'
+    # success_url = reverse_lazy('space')
+
+    # def get_queryset(self):
+    #     """
+    #     Filters bookings for the current user
+    #     """
+    #     return Booking.objects.filter(client=self.request.user)
