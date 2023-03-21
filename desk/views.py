4dc04ff5b2127fcca770ae74939d1a4504ebf318
Rashidat Adekoya <rawsheedah@yahoo.com>
2023-03-21 08:21:27 +0000

    Modify BookingFormView to redirect and reverse booking to home page after booking or rore-gin a

--- desk/views.py
+++ desk/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.http import HttpResponseRedirect
 from django.views.generic import TemplateView
 from django.views.generic import ListView
 from django.urls import reverse_lazy
+from django.contrib import messages
 
 # Booking
 from desk.models import Booking, Service
@@ -17,22 +18,48 @@ class HomePageView(TemplateView):
     template_name = 'home/index.html'
 
 
-class BookingFormView(FormView):
+class BookingListView(ListView):
     """
-    View for displaying booking availability.
+    View for users Booking
     """
     model = Booking
+    form_class = BookingForm
+    template_name = 'desk/booking_details.html'
+    # queryset = Booking.objects.filter(client=self.request.user.id)
+
+    def get_queryset(self):
+        return Booking.objects.filter(client=self.request.user.id)
+
+
+class BookingFormView(FormView):
+    """
+    View for displaying booking availability and allow user to create booking.
+    """
     form_class = BookingForm
     template_name = 'desk/space_booking.html'
 
     def post(self, request, *args, **kwargs):
         form = self.form_class(request.POST)
         if form.is_valid():
+            if self.request.user.is_authenticated:
+                form.instance.client = request.user
             # <process form cleaned data>
-            form.save()
-            return HttpResponseRedirect('/')
+                form.save()
+                messages.add_message(
+                    request,
+                    messages.SUCCESS,
+                    'Your Booking has been created successfully.')
+                return HttpResponseRedirect(reverse('home'))
+            else:
+                messages.add_message(
+                    request,
+                    messages.INFO,
+                    'Sorry!, A signin is required first.')
+                return HttpResponseRedirect(reverse('account_login'))
+        else:
+            return self.form_invalid(form)
 
-        return render(request, self.template_name, {'form': form})
+        # return render(request, self.template_name, {'form': form})
 
 
 class AboutView(TemplateView):
