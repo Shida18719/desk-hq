975842a45e415e76f15f6453b74506c3e4a89cc4
Rashidat Adekoya <rawsheedah@yahoo.com>
2023-03-21 16:33:27 +0000

    Add date input widget to booking form

--- desk/forms.py
+++ desk/forms.py
@@ -10,5 +10,8 @@ class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
         fields = [
-            'client', 'location', 'space_booking', 'booking_date',
+            'location', 'space_booking', 'booking_date',
             'booking_duration', 'booking_start', 'booking_end']
+        widgets = {
+            'booking_date': forms.DateInput(attrs={'type': 'date'})
+        }
