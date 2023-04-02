df8eee5b05f40e8d5e9b1493d6045d573942be1d
Rashidat Adekoya <rawsheedah@yahoo.com>
2023-04-02 22:33:02 +0000

    Rename maps.js to main.js and add back to top button

--- static/js/main.js
+++ static/js/main.js
@@ -11,7 +11,7 @@ function initMap() {
     const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
     //Create London's longtitude and latitude for location maps
-    var locations = [
+    const locations = [
           { lat: 51.507465, lng: 0.127809 },
           { lat: 51.483465, lng: 0.058692 },
     ];
@@ -25,7 +25,28 @@ function initMap() {
 
     // Add a marker clusterer to manage the markers.
     
-    var markerCluster = new MarkerClusterer(map, markers, 
+    const markerCluster = new MarkerClusterer(map, markers, 
     { imagePath: "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/markerclusterer/m" });
+}
 
+// Back to top button
+
+// Create toggle
+window.onscroll = () => {
+    toggleTopButton();
+}
+
+// When user clicks on the button, scroll to the top of the page with scroll behavior of smooth.
+function scrollToTop(){
+    window.scrollTo({top: 0, behavior: 'smooth'});
+}
+  
+// When user scrolls down 20px from the top of the page, display button
+function toggleTopButton() {
+    if (document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20) {
+      document.getElementById('back-to-up').classList.remove('d-none');
+    } else {
+      document.getElementById('back-to-up').classList.add('d-none');
+    }
 }
