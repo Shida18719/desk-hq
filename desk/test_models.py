89828070e766c201156bb183fedcfac64554add3
Rashidat Adekoya <rawsheedah@yahoo.com>
2023-04-07 12:54:29 +0000

    Create LocationTest class to test string representation of location_name

--- desk/test_models.py
+++ desk/test_models.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
-from .models import Booking, Enquiry
+from .models import Booking, Enquiry, Location
 from django.contrib.auth.models import User
-from django.db.models import (ForeignKey, CharField, EmailField)
+from django.db.models import (ForeignKey, CharField, EmailField, IntegerField)
 
 
 class BookingTests(TestCase):
@@ -24,8 +24,21 @@ class EnquiryTest(TestCase):
     def test_enquiry_str(self):
         enquiry_name = str(self.enquiry.name)
         enquiry_subject = str(self.enquiry.subject)
-        self.assertEqual(str(self.enquiry), f"from {enquiry_name} | subject: {enquiry_subject}")
+        self.assertEqual(
+            str(self.enquiry), f"from {enquiry_name} | subject: {enquiry_subject}")
 
+
+class LocationTest(TestCase):
+    def setUp(self):
+        reset_sequences = True
+        self.location1 = Location.objects.create(
+            location_name='DESK HQ Brooklyn House (3 STONE AVENUE LONDON SE5 2AZ')
+    
+    def test_location_name(self):
+        """Test string representation of the location_name"""
+        location1 = str(self.location1.location_name)
+        self.assertEqual(str(self.location1), (location1))
+        self.assertTrue(self.location1)
 
     # def test_fun(self):
     #     booking = self.get_object()
