4cdae9df11e063cc1b585117f794af106a9bb7c0
Rashidat Adekoya <rawsheedah@yahoo.com>
2023-04-20 17:31:51 +0000

    Deployment commit

--- deskspace/settings.py
+++ deskspace/settings.py
@@ -29,9 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-# X_FRAME_OPTIONS = 'SAMEORIGIN'
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['desk-hq.herokuapp.com', 'localhost']
 
